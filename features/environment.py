fdc7c9ca7dfe78200f7c46699769dbf2eb47cdcb
Andrii Horshkov <Andrii.Horshkov@infopulse.com>
2024-04-23 18:34:52 +0300

    Homework7

--- features/environment.py
+++ features/environment.py
@@ -3,19 +3,21 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from app.application import Application
+
 def browser_init(context):
     """
     :param context: Behave context
     """
-    driver_path = './chromedriver.exe'
-    context.driver = webdriver.Chrome(executable_path=driver_path)
+    driver_path = ChromeDriverManager().install()
+    service = Service(driver_path)
+    context.driver = webdriver.Chrome(service=service)
+
     context.driver.maximize_window()
     context.driver.implicitly_wait(6)
     context.wait = WebDriverWait(context.driver, 10)
     context.app = Application(context.driver)
-    driver_path = ChromeDriverManager().install()
-    service = Service(driver_path)
-    context.driver = webdriver.Chrome(service=service)
+    context.driver.maximize_window()
+
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
     browser_init(context)
